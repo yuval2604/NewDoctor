2c944f347a49f9b49cfb51b7ec08af23c4f3c8a6
yuval Beiser <yuvalbeiser@yuvals-MacBook-Pro.local>
2019-08-15 20:42:27 +0300

    make it better

--- public/stylesheets/js/emailsender.js
+++ public/stylesheets/js/emailsender.js
@@ -1,43 +1,40 @@
 const nodemailer = require("nodemailer");
-function emailsender(from, to, text ) {
+function emailsender(from, to, text) {
+  "use strict";
+  require("dotenv").config();
+  // async..await is not allowed in global scope, must use a wrapper
+  async function main() {
+    // Generate test SMTP service account from ethereal.email
+    // Only needed if you don't have a real mail account for testing
+    let testAccount = await nodemailer.createTestAccount();
 
-    "use strict";
-require('dotenv').config()
-// async..await is not allowed in global scope, must use a wrapper
-    async function main(){
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      service: "Gmail",
 
-        // Generate test SMTP service account from ethereal.email
-        // Only needed if you don't have a real mail account for testing
-        let testAccount = await nodemailer.createTestAccount();
+      auth: {
+        user: "yuval2604", // generated ethereal user
+        pass: "Yuval15230" // generated ethereal password
+      }
+    });
 
-        // create reusable transporter object using the default SMTP transport
-        let transporter = nodemailer.createTransport({
-            service: "Gmail",
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+      from: from, // sender address
+      to: to, // list of receivers
+      subject: "פגישה עם רופא", // Subject line
+      text: text // plain text body
+    });
 
-            auth: {
-                user: "yuval2604", // generated ethereal user
-                pass: "Yuval15230" // generated ethereal password
-            }
-        });
+    console.log("Message sent: %s", info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
-        // send mail with defined transport object
-        let info = await transporter.sendMail({
-            from: from, // sender address
-            to: to, // list of receivers
-            subject: "פגישה עם רופא", // Subject line
-            text: text // plain text body
+    // Preview only available when sending through an Ethereal account
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+  }
 
-        });
-
-        console.log("Message sent: %s", info.messageId);
-        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-        // Preview only available when sending through an Ethereal account
-        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-        // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-    }
-
-    main().catch(console.error);
+  main().catch(console.error);
 }
 
 module.exports = emailsender;
