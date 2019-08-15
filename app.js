016cbe9f7f5529ce1b4c5e6417885eb3065364b6
yuval Beiser <yuvalbeiser@yuvals-MacBook-Pro.local>
2019-08-15 21:06:30 +0300

    procfile

--- app.js
+++ app.js
@@ -1,28 +1,23 @@
 // const upload = multer({dest:'uploads/'});
 
-
 var express = require("express"),
-    app = express(),
-    bodyParser = require("body-parser"),
-    mongoose = require("mongoose"),
-    passport = require("passport"),
-    LocalStrategy = require("passport-local"),
-    methodOverride = require("method-override"),
-    Doctor = require("./models/doctor"),
-    // seedDB = require("./seeds");
+  app = express(),
+  bodyParser = require("body-parser"),
+  mongoose = require("mongoose"),
+  passport = require("passport"),
+  LocalStrategy = require("passport-local"),
+  methodOverride = require("method-override"),
+  Doctor = require("./models/doctor"),
+  // seedDB = require("./seeds");
 
-    sendemail = require("./public/stylesheets/js/emailsender");
+  sendemail = require("./public/stylesheets/js/emailsender");
 
-
-
-//requring routes    
-var
-    doctorRoutes = require("./routes/doctors")
+//requring routes
+var doctorRoutes = require("./routes/doctors");
 
 // mongoose.connect("mongodb://localhost/newdoctor");
 
 mongoose.connect("mongodb://doctor:Yuval2604@ds159546.mlab.com:59546/doctor");
-
 
 // mongoose.connect("mongodb://Yuval:Yuval2604@ds233596.mlab.com:33596/goodbrain");
 
@@ -30,10 +25,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
-app.use('/public/stylesheets/img', express.static('public/stylesheets/img'));
+app.use("/public/stylesheets/img", express.static("public/stylesheets/img"));
 
 //seedDB();
-
 
 // PASSPORT CONFIGURATION
 // app.use(require("express-session")({
@@ -48,21 +42,17 @@ app.use('/public/stylesheets/img', express.static('public/stylesheets/img'));
 // passport.deserializeUser(User.deserializeUser());
 
 app.use(function(req, res, next) {
-    res.locals.currentUser = req.user;
-    next();
+  res.locals.currentUser = req.user;
+  next();
 });
 
 app.use("/", doctorRoutes); // was indexRoutes
 
-
-
-app.listen(8000);
-
+app.listen(2000);
 
 // app.listen(process.env.PORT, process.env.IP, function() {
 //     console.log("The GoodBrain Server Has Started!");
 // });
-
 
 // mkdir data
 // echo 'mongod --bind_ip=$IP --dbpath=data --nojournal --rest "$@"' > mongod
