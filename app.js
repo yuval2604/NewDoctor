6152297c1a45f6e3f51858eac22833df42ea7847
yuval Beiser <yuvalbeiser@yuvals-MacBook-Pro.local>
2019-08-18 20:51:48 +0300

    firebase

--- app.js
+++ app.js
@@ -48,7 +48,11 @@ app.use(function(req, res, next) {
 
 app.use("/", doctorRoutes); // was indexRoutes
 
-app.listen(2000);
+const port = process.env.PORT || 2000;
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
 
 // app.listen(process.env.PORT, process.env.IP, function() {
 //     console.log("The GoodBrain Server Has Started!");
